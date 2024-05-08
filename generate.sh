5feb96a0991deca8ac96e9365a0a27c5fbd6c47a
KodiKraig <cody.craig@aol.com>
2024-05-08 00:28:25 -0400

    Initial generation

--- generate.sh
+++ generate.sh
@@ -1,8 +1,7 @@
-npx @openapitools/openapi-generator-cli generate --skip-validate-spec -i ./swagger/swagger.yaml \
+npx @openapitools/openapi-generator-cli generate -i ./swagger/swagger.yaml \
     -g typescript-fetch -o ./ \
     --additional-properties=npmName=blockscout-typescript-fetch \
     --additional-properties=npmVersion=1.0.0 \
     --additional-properties=supportsES6=true \
     --additional-properties=withInterfaces=true \
-    --additional-properties=useSingleRequestParameter=true \
-    --additional-properties=prefixParameterInterfaces=true \
+    --additional-properties=useSingleRequestParameter=true
